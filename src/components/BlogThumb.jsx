68b780b453c78d3b9b208fe8b4c2909af5fd0e79
Josh S <aaabccc87@protonmail.com>
2023-08-11 12:56:52 -0400

    Basic component to display info about a Blog

---
+++ src/components/BlogThumb.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { unescape } from 'validator'
+
+function BlogThumb({ blog }) {
+  // title description  owner  created
+  return (
+    <div>
+      <p><Link to={`/${blog.name}`}>{unescape(blog.title)}</Link></p>
+      <p>{unescape(blog.description)}</p>
+      <p><Link to={`/authors/${unescape(blog.owner.username)}`}>{unescape(blog.owner.username)}</Link></p>
+    </div>
+  )
+}
+
+export default BlogThumb;

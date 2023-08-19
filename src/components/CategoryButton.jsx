8556572dfe5e02ad112e802656bd0b671c6a4357
Josh S <aaabccc87@protonmail.com>
2023-08-18 20:58:22 -0400

    Fix link path

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -5,7 +5,7 @@ import { unescape } from 'validator';
 function CategoryButton({ category }) {
   return (
     <span>
-      <Link to={`./categories/${category._id}`}>
+      <Link to={`/categories/${category._id}`}>
         { unescape(category.name) }
       </Link>
     </span>

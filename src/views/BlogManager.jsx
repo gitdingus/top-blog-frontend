8e6b4c088ea7c9ca250ce9d7371b86ceef380b47
Josh S. <aaabccc87@protonmail.com>
2023-09-07 12:24:59 -0400

    Modify markup for styling; add link to view users blog posts

--- src/views/BlogManager.jsx
+++ src/views/BlogManager.jsx
@@ -1,17 +1,27 @@
-import React, { useContext } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext.jsx';
+import getLoggedInUser from '../utils/getLoggedInUser.js';
+import styles from '../styles/blog-manager.module.css';
 
 function BlogManager() {
-  const { currentUser } = useContext(UserContext);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const location = useLocation();
+  
+  if (currentUser === null) {
+    return <div>Log in to access this page</div>
+  }
 
   return (
-    <div>
-      <ul>
-        <li><Link to="create-blog">Create Blog</Link></li>
-        <li><Link to="blogs">View Blogs</Link></li>
+    <div className={styles.blogManager}>
+      <ul className={styles.blogOptions}>
+        <li className={location.pathname.match(/create-blog/) !== null ? styles.active : ''}><Link to="create-blog">Create Blog</Link></li>
+        <li className={location.pathname.match(/\/blogs$/) !== null ? styles.active : ''}><Link to="blogs">View Blogs</Link></li>
+        <li className={location.pathname.match(/\/blogposts$/) !== null ? styles.active : ''}><Link to="blogposts">View Blog Posts</Link></li>
       </ul>
-      <Outlet />
+      <div className={styles.screen}>
+        <Outlet />
+      </div>
     </div>
   )
 }

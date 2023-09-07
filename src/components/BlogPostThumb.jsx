c2d542e59c9c8317c7741622903f7fab0b258a1d
Josh S. <aaabccc87@protonmail.com>
2023-09-07 12:19:26 -0400

    add styling + display authors name if provided

--- src/components/BlogPostThumb.jsx
+++ src/components/BlogPostThumb.jsx
@@ -3,18 +3,26 @@ import { Link } from 'react-router-dom';
 import { unescape } from 'validator';
 import CategoryButton from './CategoryButton.jsx';
 
+import '../styles/blogpostthumb.css';
+
 function BlogPostThumb({ post }) {
   return (
-    <div className="blog-post">
-      <p>
+    <div className="blog-post-thumb">
+      <p className="author-category">
         <span>
-          <Link to={`./authors/${post.author.username}`}>{unescape(post.author.username)}</Link>
+          <Link to={`./authors/${post.author.username}`}>{
+            (
+              post.author.firstName !== undefined && post.author.lastName !== undefined
+              && `${post.author.firstName} ${post.author.lastName}`
+            ) ||
+            post.author.username
+          }</Link>
         </span>
         &nbsp;in&nbsp; 
         <CategoryButton category={post.blog.category} />
       </p>
-      <p><Link to={`./${post.blog.name}/${post._id}`}>{unescape(post.title)}</Link></p>
-      <p>posted on: {new Date(post.created).toLocaleString()}</p>
+      <p className="title"><Link to={`./${post.blog.name}/${post._id}`}>{unescape(post.title)}</Link></p>
+      <p className="date">posted on: {new Date(post.created).toLocaleDateString()}</p>
     </div>
   )
 }

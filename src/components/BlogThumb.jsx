8c45ddaa97d702f6999af1affb22819d612b40a3
Josh S. <aaabccc87@protonmail.com>
2023-09-07 12:18:38 -0400

    add margins

--- src/components/BlogThumb.jsx
+++ src/components/BlogThumb.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { unescape } from 'validator'
+import styles from '../styles/blog-thumb.module.css';
 
 function BlogThumb({ blog }) {
   // title description  owner  created
   return (
-    <div>
+    <div className={styles.blogThumb}>
       <p><Link to={`/${blog.name}`}>{unescape(blog.title)}</Link></p>
       <p>{unescape(blog.description || blog.preview)}</p>
-      <p><Link to={`/authors/${unescape(blog.owner.username)}`}>{unescape(blog.owner.username)}</Link></p>
+      <p><Link to={`/authors/${unescape(blog.owner.username)}`}>{
+        blog.owner.firstName !== undefined && blog.owner.lastName !== undefined
+        && unescape(`${blog.owner.firstName} ${blog.owner.lastName}`)
+        || unescape(blog.owner.username)
+      }</Link></p>
     </div>
   )
 }

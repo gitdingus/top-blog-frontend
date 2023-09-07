4857b439f8c8ac3b63ca1eec0a61b40778268346
Josh S. <aaabccc87@protonmail.com>
2023-09-07 12:17:57 -0400

    add margins

--- src/components/CategoryThumb.jsx
+++ src/components/CategoryThumb.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { unescape } from 'validator';
+import styles from '../styles/category-thumb.module.css';
 
 function CategoryThumb({ category }) {
   return (
-    <div className="category">
-    <h2>
-      <Link to={`/categories/${category._id}`}>{unescape(category.name) }</Link>
-    </h2>
-    <p>{ unescape(category.description) }</p>
+    <div className={styles.categoryThumb}>
+      <h2>
+        <Link to={`/categories/${category._id}`}>{unescape(category.name) }</Link>
+      </h2>
+      <p>{ unescape(category.description) }</p>
     </div>
   )
 }

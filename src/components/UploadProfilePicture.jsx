d950f03986e2a9693b372034c4c3ea4246c214a0
Josh S. <aaabccc87@protonmail.com>
2023-10-05 11:14:56 -0400

    Form to upload or delete profile picture

--- src/components/UploadProfilePicture.jsx
+++ src/components/UploadProfilePicture.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import { UserContext } from '../contexts/UserContext.jsx';
+import '../styles/single-column-grid-form.css';
 
 function UploadProfilePicture() {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const uploadPhotoForm = useRef(null);
+
   return(
-    <div>Upload picture!</div>
+    <div className="form-area single-column-grid-form">
+      <form ref={uploadPhotoForm} encType="multipart/form" onSubmit={(e) => {
+        e.preventDefault();
+
+        const formData = new FormData(uploadPhotoForm.current);
+        
+        fetch(`http://localhost:3000/api/users/${currentUser._id}/upload-photo`, {
+          method: 'post',
+          credentials: 'include',
+          body: formData,
+        })
+          .then((res) => {
+            console.log(res.status);
+          })
+          .catch((err) => console.log(err));
+      }}>
+        <label htmlFor="photoInput">Profile Picture</label>
+        <input type="file" id="photoInput" name="photo" />
+        <p>
+          <label htmlFor="delete_pic">Delete Profile Pic</label>
+          <input type="checkbox" id="delete_pic" name="delete_pic" />
+        </p>
+        <button type="submit">Upload Photo</button>
+      </form>
+    </div>
   )
 }
 
